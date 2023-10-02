497030b4372995429ea6f58e24983367fbebdc8d
Megan <esler6724@gmail.com>
2023-10-02 16:58:54 -0700

    commit tree 5146bb8404f2aa05b60ce064e966355283d5e733

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,33 @@
 class ApplicationController < Sinatra::Base
   set default_content_type: "application/json"
   
+  delete '/baked_goods/:id' do
+    good = BakedGood.find(params[:id])
+    good.destroy
+    good.to_json
+  end
+
+  patch '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.update(
+      name: params[:name]
+    )
+    bakery.to_json
+  end
+
+  post '/baked_goods' do
+    baked_goods = BakedGood.create(
+      name: params[:name],
+      price: params[:price],
+      bakery_id: params[:bakery_id],
+    )
+    # t.string :name
+    # t.integer :price
+    # t.integer :bakery_id
+    # t.timestamps
+    baked_goods.to_json
+  end
+
   get '/bakeries' do
     bakeries = Bakery.all
     bakeries.to_json
